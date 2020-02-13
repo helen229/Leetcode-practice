d784a7e9ed25786c9006341061f57816d1a3f223
Han Gao <gaohan9409@gmail.com>
2020-02-12 19:17:30 -0500

    add 1323

---
+++ 1323.java
@@ -0,0 +1,15 @@
+//review the translation between char and string and integer.
+class Solution {
+    public int maximum69Number (int num) {
+        //******
+        char[] chars = Integer.toString(num).toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '6') {
+                chars[i] = '9';
+                break;
+            }
+        }
+        //******
+        return Integer.parseInt(new String(chars));
+    }
+}
