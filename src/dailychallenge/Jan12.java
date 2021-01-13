39d75ee4016dcc0bcb2ddec9c07065ee7e54d2bb
helen229 <gaohan9409@gmail.com>
2021-01-12 23:45:30 -0500

    Daily challenge update

---
+++ src/dailychallenge/Jan12.java
@@ -0,0 +1,30 @@
+package dailychallenge;
+
+public class Jan12 {
+    /**
+     * I'm back!
+     * a very straightforward linked list problem
+     *
+     * Input: l1 = [2,4,3], l2 = [5,6,4]
+     * Output: [7,0,8]
+     * Explanation: 342 + 465 = 807.
+     * 
+     **/
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            ListNode cur = new ListNode(0);
+            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return head.next;
+    }
+}
